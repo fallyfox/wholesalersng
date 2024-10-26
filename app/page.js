9a86934b30097f0385d8ad9f5950bd89244e1d2b
Joseph Ogbu <fallyfox@gmail.com>
2024-10-26 15:38:21 +0100

    install part of homepage

--- app/page.js
+++ app/page.js
@@ -1,101 +1,117 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <main className="p-4 md:px-8 lg:px-16">
+        <header className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 md:gap-6">
+            <div className="bg-gradient-to-b from-orange-400 via-yellow-500 to-orange-600 p-6 rounded-lg">
+                <p className="border border-orange-500 p-3 rounded-lg text-center text-lg lg:text-2xl text-orange-900 mb-5">Hot Categories</p>
+                <div className="grid grid-cols-2 gap-3">
+                    <blockquote className="h-28 flex flex-col justify-center items-center gap-3 bg-white p-3 rounded-lg">
+                        <p className="w-12 h-12 rounded-full bg-black"></p>
+                        <Link href="#">Cloths</Link>
+                    </blockquote>
+                    <blockquote className="h-28 flex flex-col justify-center items-center gap-3 bg-white p-3 rounded-lg">
+                        <p className="w-12 h-12 rounded-full bg-black"></p>
+                        <Link href="#">Footwears</Link>
+                    </blockquote>
+                    <blockquote className="h-28 flex flex-col justify-center items-center gap-3 bg-white p-3 rounded-lg">
+                        <p className="w-12 h-12 rounded-full bg-black"></p>
+                        <Link href="#">Raw Food</Link>
+                    </blockquote>
+                    <blockquote className="h-28 flex flex-col justify-center items-center gap-3 bg-white p-3 rounded-lg">
+                        <p className="w-12 h-12 rounded-full bg-black"></p>
+                        <Link href="#">Electronics</Link>
+                    </blockquote>
+                </div>
+            </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+            <div className="flex flex-col gap-2 border border-gray-200 shadow-md rounded-lg">
+               <Image 
+               width={400}
+               height={400}
+                className="w-full h-[220px] rounded-lg" 
+                src="/images/nigeria.jpg" 
+                alt="product image"/>
+                <ul className="flex items-center gap-2 px-2">
+                    <li className="bg-green-600 text-green-100 p-1 rounded-full text-[8px]">Sponsored</li>
+                    <li className="bg-orange-600 text-yellow-100 p-1 rounded-full text-[8px]">Hot</li>
+                    <li className="text-gray-600">Wireless Bluetooth 5.0 Gaming Mouse</li>
+                </ul>
+                <ul className="flex justify-between gap-2 px-2">
+                    <li className="text-xl text-gray-600">₦12,600</li>
+                    <li className="flex flex-col items-center">
+                        <span className="text-xs text-gray-400 uppercase">Supply</span>
+                        <span className="text-md text-gray-600">760</span>
+                    </li>
+                    <li className="flex flex-col items-center">
+                        <span className="text-xs text-gray-400 uppercase">MOQ</span>
+                        <span className="text-md text-gray-600">2</span>
+                    </li>
+                </ul>
+                <p className="text-xs text-gray-600 px-2">Available in black, white, and green colours. Customization and branding are available on demand</p>
+                <p className="px-2 pb-4">
+                    <Link className="bg-orange-600 text-yellow-100 text-xs text-center p-2 rounded-lg border-b-4 border-green-600" href="#">Open Details</Link>
+                </p>
+            </div>
+            <div className="flex flex-col gap-2 border border-gray-200 shadow-md rounded-lg">
+               <Image 
+               width={400}
+               height={400}
+                className="w-full h-[220px] rounded-lg" 
+                src="/images/nigeria.jpg" 
+                alt="product image"/>
+                <ul className="flex items-center gap-2 px-2">
+                    <li className="bg-green-600 text-green-100 p-1 rounded-full text-[8px]">Sponsored</li>
+                    <li className="bg-orange-600 text-yellow-100 p-1 rounded-full text-[8px]">Hot</li>
+                    <li className="text-gray-600">Wireless Bluetooth 5.0 Gaming Mouse</li>
+                </ul>
+                <ul className="flex justify-between gap-2 px-2">
+                    <li className="text-xl text-gray-600">₦12,600</li>
+                    <li className="flex flex-col items-center">
+                        <span className="text-xs text-gray-400 uppercase">Supply</span>
+                        <span className="text-md text-gray-600">760</span>
+                    </li>
+                    <li className="flex flex-col items-center">
+                        <span className="text-xs text-gray-400 uppercase">MOQ</span>
+                        <span className="text-md text-gray-600">2</span>
+                    </li>,
+                </ul>
+                <p className="text-xs text-gray-600 px-2">Available in black, white, and green colours. Customization and branding are available on demand</p>
+                <p className="px-2 pb-4">
+                    <Link className="bg-orange-600 text-yellow-100 text-xs text-center p-2 rounded-lg border-b-4 border-green-600" href="#">Open Details</Link>
+                </p>
+            </div>
+            <div className="flex flex-col gap-2 border border-gray-200 shadow-md rounded-lg">
+               <Image 
+               width={400}
+               height={400}
+                className="w-full h-[220px] rounded-lg" 
+                src="/images/nigeria.jpg" 
+                alt="product image"/>
+                <ul className="flex items-center gap-2 px-2">
+                    <li className="bg-green-600 text-green-100 p-1 rounded-full text-[8px]">Sponsored</li>
+                    <li className="bg-orange-600 text-yellow-100 p-1 rounded-full text-[8px]">Hot</li>
+                    <li className="text-gray-600">Wireless Bluetooth 5.0 Gaming Mouse</li>
+                </ul>
+                <ul className="flex justify-between gap-2 px-2">
+                    <li className="text-xl text-gray-600">₦12,600</li>
+                    <li className="flex flex-col items-center">
+                        <span className="text-xs text-gray-400 uppercase">Supply</span>
+                        <span className="text-md text-gray-600">760</span>
+                    </li>
+                    <li className="flex flex-col items-center">
+                        <span className="text-xs text-gray-400 uppercase">MOQ</span>
+                        <span className="text-md text-gray-600">2</span>
+                    </li>
+                </ul>
+                <p className="text-xs text-gray-600 px-2">Available in black, white, and green colours. Customization and branding are available on demand</p>
+                <p className="px-2 pb-4">
+                    <Link className="bg-orange-600 text-yellow-100 text-xs text-center p-2 rounded-lg border-b-4 border-green-600" href="#">Open Details</Link>
+                </p>
+            </div>
+        </header>
+    </main>
   );
 }
