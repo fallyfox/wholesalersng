d2dbbbc1dd0089a580d804fede9f94cbc502c61d
Joseph Ogbu <fallyfox@gmail.com>
2024-11-02 13:53:47 +0100

    footer sizing changes

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ const currentYear = new Date().getFullYear()
 
 export function Footer () {
     return (
-        <footer className="h-[68px] flex flex-col md:flex-row justify-between md:items-center bg-gray-300 py-3 px-2 md:px-12 lg:px-16">
+        <footer className="min-h-[68px] lg:h-[68px] flex flex-col md:flex-row justify-between md:items-center bg-gray-300 py-3 px-2 md:px-12 lg:px-16">
             <div>
                 <p className="text-xl text-gray-800 mb-1">Wholesalers NG</p>
                 <p className="text-gray-600 text-xs">&copy; {currentYear} All Rights Reserved</p>
