ec418ead955f9d79b88bf9c915438c24354a7cea
Joseph Ogbu <fallyfox@gmail.com>
2024-11-08 13:29:27 +0100

    Google authentication setup

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { Footer } from "@/components/Footer";
 import { Navbar } from "@/components/Navbar";
+import { SessionProvider } from "next-auth/react";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -25,9 +26,11 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+       <SessionProvider>
         <Navbar/>
         {children}
         <Footer/>
+       </SessionProvider>
       </body>
     </html>
   );
